712b87cc0ef898b4ac9eed74ee27ddd7ff6eafc2
Matu <90573043+Ma1u0@users.noreply.github.com>
2025-12-14 00:38:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ L.marker([55.6181, 12.6508], { icon: droneOrange })
   .bindPopup(`
     <b> Copenhagen Airport, Denmark 🇩🇰 </b> <br>
     <br>
-    <b>Date/Time:</b> 22 Sep 2025, ~20:26–00:30<br>
+    <b>Date/Time:</b> 22 Sep 2025; ~20:26–00:30<br>
     <b>Type:</b> Drone sighting<br>
     <b>Details:</b> Copenhagen Airport was temporarily closed after 2–3 drones were spotted.<br>
     Danish authorities stated the drones were operated by a "capable actor". <br>
